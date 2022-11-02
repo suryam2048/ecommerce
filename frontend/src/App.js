e7916faab811194d79bbb02748c55b6be23e91a3
Suryam2048 <suryamsourya8@gmail.com>
2022-11-02 11:46:16 +0530

    react app

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">
+          Artwork
+        </a>
+      </header>
+      <main>
+        List product
+      </main>
+    </div>
+  );
+}
+
+export default App;
