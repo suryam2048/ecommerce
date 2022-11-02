bf9e5b8c5e323309b2abf17a3ea200f10a2fff51
Suryam2048 <suryamsourya8@gmail.com>
2022-11-02 18:03:38 +0530

    Add Route And product

---
+++ frontend/src/data.js
@@ -0,0 +1,53 @@
+const data = {
+    products: [
+        {
+            name: 'Beauty of Nature',
+            slug: ' Beauty of nature  ART',
+            category: 'Nature',
+            price: 130,
+            countInStock: 10,
+            image: '/images/5936217-HSC00002-25.jpg',
+            By: 'suryam01',
+            rating: 4.4,
+            numReviews: 3,
+            Decription: 'It  realize how wonder the nature is'
+        },
+        {
+            name: 'Beauty of Nature',
+            slug: ' Book World Design',
+            category: 'Love',
+            price: 130,
+            countInStock: 10,
+            image: '/images/WOrld.jpg',
+            By: 'suryam02',
+            rating: 4.4,
+            numReviews: 3,
+            Decription: 'It  realize how wonder the nature is'
+        },
+        {
+            name: 'Beauty of Nature',
+            slug: ' Patterns',
+            category: '3d',
+            price: 130,
+            countInStock: 10,
+            image: '/images/index.jpg',
+            By: 'suryam03',
+            rating: 4.4,
+            numReviews: 3,
+            Decription: 'It  realize how wonder the nature is'
+        },
+        {
+            name: 'Art of a Girl',
+            slug: 'Art of a Girl',
+            category: 'world',
+            price: 130,
+            countInStock: 10,
+            image: '/images/giftguidetile.jpg',
+            By: 'suryam04',
+            rating: 4.4,
+            numReviews: 3,
+            Decription: 'It  realize how wonder the nature is'
+        },
+    ],
+};
+export default data;
