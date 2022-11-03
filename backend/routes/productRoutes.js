48b7b841ed5f54dbe82163dbb21b5461a9a984c6
Suryam2048 <suryamsourya8@gmail.com>
2022-11-03 20:39:35 +0530

    ero

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Product from '../models/productModel.js';
+import expressAsyncHandler from 'express-async-handler';
 
 const productRouter = express.Router();
 
@@ -8,6 +9,13 @@ productRouter.get('/', async (req, res) => {
   res.send(products);
 });
 
+productRouter.get(
+  '/categories',
+  expressAsyncHandler(async (req, res) => {
+    const categories = await Product.find().distinct('category');
+    res.send(categories);
+  })
+);
 
 productRouter.get('/slug/:slug', async (req, res) => {
   const product = await Product.findOne((x) => x.slug === req.params.slug);
