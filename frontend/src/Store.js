4fb96e27196e309ea188e5d38e4c5587479320ee
Suryam2048 <suryamsourya8@gmail.com>
2022-11-03 16:12:43 +0530

    payment and order screen

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -6,10 +6,14 @@ const initialState = {
   userInfo: localStorage.getItem('userInfo')
   ? JSON.parse(localStorage.getItem('userInfo'))
   : null,
+
   cart: {
     shippingAddress: localStorage.getItem('shippingAddress')
     ? JSON.parse(localStorage.getItem('shippingAddress'))
     : {},
+    paymentMethod: localStorage.getItem('paymentMethod')
+      ? localStorage.getItem('paymentMethod')
+      : '',
     cartItems: localStorage.getItem('cartItems')
     ? JSON.parse(localStorage.getItem('cartItems'))
     : [],
@@ -47,6 +51,7 @@ function reducer(state, action) {
           cart: {
             cartItems: [],
             shippingAddress: {},
+            paymentMethod: '',
           },
         };
       case 'SAVE_SHIPPING_ADDRESS':
@@ -57,6 +62,11 @@ function reducer(state, action) {
             shippingAddress: action.payload,
           },
         };
+        case 'SAVE_PAYMENT_METHOD':
+          return {
+            ...state,
+            cart: { ...state.cart, paymentMethod: action.payload },
+          };
     default:
       return state;
   }
